edd17c12b6d8ec4da72cfc96e592eb35aea3acf2
Miguel Attie <attiemiguel751@gmail.com>
2025-02-20 05:44:57 -0300

    update

--- expo-app/StickerSmash/app/index.tsx
+++ expo-app/StickerSmash/app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.conteiner}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
     </View>
-  );
-}
+  )}
+
+    const styles = StyleSheet.create({
+      conteiner: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      text: {
+        color: 'white',
+      },
+    });
+
