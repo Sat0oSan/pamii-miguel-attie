d4da81ecdba5525b24558ef727315da5992a996f
Miguel Attie <attiemiguel751@gmail.com>
2025-02-25 01:46:29 -0300

    update StickerSmash

--- expo-app/StickerSmash/app/index.tsx
+++ expo-app/StickerSmash/app/index.tsx
@@ -1,21 +1,29 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.conteiner}>
-      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Link href={'/about'} style={styles.button}>Ir para a tela Sobre</Link>
     </View>
-  )}
+  );
+}
 
-    const styles = StyleSheet.create({
-      conteiner: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      text: {
-        color: 'white',
-      },
-    });
 
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#25292e',
+      flex: 1, 
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      color: '#fff'
+    },
+    button:{
+      fontSize: 20,
+      color: '#fff',
+      textDecorationLine: 'underline',
+    }
+})
